dd0cfc5bd2087e8bbdf5f49df390fe655a85030c
danielperzales <dani3l122@hotmail.com>
2016-01-20 12:54:25 -0500

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+def using_push(array , y)
+  array1 = ["come", "te","este"]
+  nuevo = "Niger"
+  array1.push(nuevo)
+end
+
+def using_unshift(x,y)
+  arra = ["come","te", "esta", "como","te"]
+  arr = "Brooklyn Heights"
+  arra.unshift(arr)
+end
+
+def using_pop(perro)
+  perro.pop
+end
+def pop_with_args(comelon)
+  comelon.pop(2)
+end
+def using_shift(ere)
+  ere.shift
+  end
+  def shift_with_args(carr)
+    carr.shift(2)
+  end
+  def using_concat(array ,array1)
+    array.concat(array1)
+  end
+  def using_insert(array , element)
+    array.insert(4,element)
+  end
+  def using_uniq(are)
+   are.uniq
+  end
+  def using_flatten(arra)
+    arra.flatten 
+  end
+  def using_delete(array, string)
+    array.delete(string)
+  end
+  def using_delete_at(r,integer)
+    r.delete_at(integer)
+  end
+
